afd3a1553f4defdc183667ed43f4b31c269f9cd7
Ion Stici <ionsticidev@gmail.com>
2023-10-10 17:03:44 +0300

    Add Meetup-Events Project from Section 4

---
+++ 01-meetup-events/components/ui/button.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button(props) {
+    if (props.link) {
+        return (
+            <Link className={classes.btn} href={props.link}>
+                {props.children}
+            </Link>
+        );
+    }
+
+    return (
+        <button onClick={props.onClick} className={classes.btn}>
+            {props.children}
+        </button>
+    );
+}
+
+export default Button;
