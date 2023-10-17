3f1999bf1d1102ea20e540487894503a78d7c15c
Ion Stici <ionsticidev@gmail.com>
2023-10-17 16:05:04 +0300

    Format imported markdown content

--- nextjs-blog-code-along/components/posts/post-detail/post-content.js
+++ nextjs-blog-code-along/components/posts/post-detail/post-content.js
@@ -1,16 +1,59 @@
+import Image from "next/image";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+
 import ReactMarkdown from "react-markdown";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
+
+// https://github.com/mschwarzmueller/nextjs-course-code/compare/3b99cd0...a9fb64b#diff-3db12f592d8426efcf0708bcecc9d9a283394c9089879adcb7bdca47ac060ce8L34
