9b01562bd9108fccd8b277effed63c34fa4419fc
Ion Stici <ionsticidev@gmail.com>
2023-10-19 10:23:27 +0300

    Upate next-auth

--- next-auth-code-along/components/auth/auth-form.js
+++ next-auth-code-along/components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/react";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -38,7 +39,13 @@ function AuthForm() {
     // validation
 
     if (isLogin) {
-      // log user in
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
--- next-auth-code-along/components/layout/main-navigation.js
+++ next-auth-code-along/components/layout/main-navigation.js
@@ -1,8 +1,19 @@
-import Link from 'next/link';
+import Link from "next/link";
+import classes from "./main-navigation.module.css";
 
-import classes from './main-navigation.module.css';
+import { useSession, signOut } from "next-auth/react";
 
 function MainNavigation() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
+  //   console.log(loading);
+  //   console.log(session);
+
+  function logoutHandler() {
+    signOut();
+  }
+
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -10,15 +21,23 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
